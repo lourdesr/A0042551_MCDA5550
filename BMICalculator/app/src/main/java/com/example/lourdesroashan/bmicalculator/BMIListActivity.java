5ca0d7361d44a986634b22b729c1825349ab9ac4
Lourdes Roashan <root.roashan@gmail.com>
2018-07-18 21:59:50 -0300

    Huge DB commit

---
+++ BMICalculator/app/src/main/java/com/example/lourdesroashan/bmicalculator/BMIListActivity.java
@@ -0,0 +1,15 @@
+package com.example.lourdesroashan.bmicalculator;
+
+import android.app.ListActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BMIListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_bmilist);
+
+    }
+}
--- BMICalculator/app/src/main/java/com/example/lourdesroashan/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/lourdesroashan/bmicalculator/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.lourdesroashan.bmicalculator;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        DatabaseHelper helper = new DatabaseHelper(this);
+//        SQLiteDatabase db = helper.getWritableDatabase();
+//
+//        Cursor cursor = db.query(DatabaseHelper.TABLE_NAME, new String[] {"NAME","PASSWORD","DATE"},
+//                null,null,null,null,null);
+//
+//
+//        if(cursor.moveToFirst()){
+//            String name = cursor.getString(0);
+//
+//
+//            EditText res = findViewById(R.id.textView7);
+//            results.setText(name);
+//        }
+
     }
 
     public void goToCalculate(View view)
     {
-        Intent intent = new Intent( this,Calculate.class);
+        Intent intent = new Intent( this,RegisterActivity.class);
         startActivity(intent);
     }
 }
